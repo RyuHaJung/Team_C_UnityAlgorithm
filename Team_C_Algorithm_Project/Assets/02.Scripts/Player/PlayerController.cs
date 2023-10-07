0e1d540fc2a39127542d2bb446dffe1e11f98282
RyuHaJung <red061011@gmail.com>
2023-10-07 15:51:20 +0900

    Feat:"플레이어 hp, 아이템, 문 스크립트 작성, 아이템 오브젝트 만듬
    플레이어 컨트롤에 hp추가 하트, 열쇠 아이템 만듬 열쇠 소지시 문 열리도록

--- Team_C_Algorithm_Project/Assets/02.Scripts/Player/PlayerController.cs
+++ Team_C_Algorithm_Project/Assets/02.Scripts/Player/PlayerController.cs
@@ -25,6 +25,11 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rbody;              //Rigidbody 2D
     bool isMoving = false;          //이동 중인지 여부
 
+    //데미지 처리
+    public static int hp = 3;           //플레이어 HP
+    public static string gameState;     //게임 상태
+    bool inDamage = false;              //데미지 받는 중
+
     // Use this for initialization
     void Start()
     {
@@ -32,12 +37,18 @@ public class PlayerController : MonoBehaviour
         rbody = GetComponent<Rigidbody2D>();
         //애니메이션
         oldAnimation = downAnime;
+        //게임 상태를 플레이 중으로 하기
+        gameState = "playing";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 게임중이 아니거나 데미지를 받는 중엔 아무것도 하지 않는다.
+        if (gameState != "playing" || inDamage)
+        {
+            return;
+        }
         if (isMoving == false)
         {
             axisH = Input.GetAxisRaw("Horizontal"); //좌우 키 입력
@@ -77,6 +88,28 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        //게임중이 아니면 아무것도 하지 않는다.
+        if (gameState != "playing")
+        {
+            return;
+        }
+        if (inDamage)
+        {
+            //데미지 받는 중엔 점멸 시키기
+            float val = Mathf.Sin(Time.time * 50);
+            Debug.Log(val);
+            if (val > 0)
+            {
+                //스프라이트 표시
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else
+            {
+                //스프라이트 비표시
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            }
+            return; //데미지 받는 중엔 조작을 할수 없게하기
+        }
         //이동 속도 변경하기
         rbody.velocity = new Vector2(axisH, axisV) * speed;
     }
@@ -116,5 +149,73 @@ public class PlayerController : MonoBehaviour
         }
         return angle;
     }
-    
+
+    //접촉 (물리)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            GetDamage(collision.gameObject);
+        }
+    }
+
+    //데미지
+    void GetDamage(GameObject enemy)
+    {
+        if (gameState == "playing")
+        {
+            hp--;   //HP감소
+            //HP 갱신
+            PlayerPrefs.SetInt("PlayerHP", hp);
+            if (hp > 0)
+            {
+                //이동 중지
+                rbody.velocity = new Vector2(0, 0);
+                //적 캐릭터의 반대 방향으로 히트백 
+                Vector3 toPos = (transform.position - enemy.transform.position).normalized;
+                rbody.AddForce(new Vector2(toPos.x * 4,
+                                           toPos.y * 4),
+                                           ForceMode2D.Impulse);
+                //데미지 받는 중으로 설정
+                inDamage = true;
+                Invoke("DamageEnd", 0.25f);
+
+            }
+            else
+            {
+                //게임 오버
+                GameOver();
+            }
+        }
+    }
+    //데미지 받기 끝
+    void DamageEnd()
+    {
+        //데미지 받는 중이 아님으로 설정
+        inDamage = false;
+        //스프라이트 되돌리기
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    }
+    //게임 오버
+    void GameOver()
+    {
+        Debug.Log("게임 오버!");
+        gameState = "gameover";
+        //=====================
+        //게임 오버 연출
+        //=====================
+        //플레이어 충돌 판정 비활성
+        GetComponent<CircleCollider2D>().enabled = false;
+        //이동 중지
+        rbody.velocity = new Vector2(0, 0);
+        //중력을 적용하여 플레이어를 위로 튀어오르게하는 연출
+        rbody.gravityScale = 1;
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        // 애니메이션 변경하기
+        GetComponent<Animator>().Play(deadAnime);
+        //1초후에 플레이어 캐릭터 제거하기
+        Destroy(gameObject, 1.0f);
+
+    }
+
 }
